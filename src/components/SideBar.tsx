184390f05b8447dd01c1583efbe6ab08d73ed7b2
voitaraujo <voitilaaraujo@gmail.com>
2021-09-18 19:24:32 -0300

    add challenge resolution

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface IhandleClickButtonProps {
+  updateGenreHandle: (id: number) => void
+  genres: GenreResponseProps[]
+  selectedGenreId: number
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: IhandleClickButtonProps) {
+
+  function handleClickButton(id: number) {
+    props.updateGenreHandle(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
